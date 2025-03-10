719a49ab1eda76ad5c5b20f13cae50a59e1aab2f
Study9 Trainer <trainer@study9.com>
2025-03-11 02:10:42 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hi, GitHub Actions!');
+    res.send('Hi, GitHub Actions .');
 });
 
 const PORT = process.env.PORT || 3000;
